afbdb4a582fa3e7fc1ce6f491bb96b04c193a2ef
drayata <profirstindra@gmail.com>
2026-08-02 22:58:08 +0700

    set state best practices

--- react-course/steps/src/App.js
+++ react-course/steps/src/App.js
@@ -12,17 +12,17 @@ const App = () => {
 
   const handlePrev = () => {
     if (step === 1) return;
-    setStep(step - 1);
+    setStep((s) => s - 1);
   };
 
   const handleNext = () => {
     if (step === 3) return;
-    setStep(step + 1);
+    setStep((s) => s + 1);
   };
 
   return (
     <>
-      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
         &times;
       </button>
       {isOpen && (
