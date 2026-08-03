495476b6b341f5d45580cb1d99ef3e403bdaf7cb
drayata <profirstindra@gmail.com>
2026-08-03 11:42:17 +0700

    render item list (71)

--- react-course/farAway/src/App.js
+++ react-course/farAway/src/App.js
@@ -1,6 +1,11 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+];
+
 const App = () => {
   return (
-    <div className="App">
+    <div className="app">
       <Logo />
       <Form />
       <PackingList />
@@ -22,12 +27,31 @@ const Form = () => {
 };
 
 const PackingList = () => {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item items={item} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const Item = ({ items }) => {
+  return (
+    <li>
+      <span style={items.packed ? { textDecoration: "line-through" } : {}}>
+        {items.quantity} {items.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 };
 
 const Stats = () => {
   return (
-    <footer>
+    <footer className="stats">
       <em>You have X items on your list, and you already packed X (X%)</em>
     </footer>
   );
