5ac41eb43035f4ab71817881ea2ed2d228a9e51c
drayata <profirstindra@gmail.com>
2026-08-05 13:29:37 +0700

    date counter challenge v2

--- react-course/dateCounterChallenge/src/App.js
+++ react-course/dateCounterChallenge/src/App.js
@@ -14,14 +14,6 @@ const Counter = () => {
   const date = new Date();
   date.setDate(date.getDate() + count);
 
-  const handleSubsStep = () => {
-    setStep((s) => s - 1);
-  };
-
-  const handleAddStep = () => {
-    setStep((s) => s + 1);
-  };
-
   const handleSubsCount = () => {
     setCount((c) => c - step);
   };
@@ -33,13 +25,22 @@ const Counter = () => {
   return (
     <div>
       <div className="step">
-        <button onClick={handleSubsStep}>-</button>
-        Step: {step}
-        <button onClick={handleAddStep}>+</button>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+        <span>{step}</span>
       </div>
       <div className="count">
         <button onClick={handleSubsCount}>-</button>
-        Count: {count}
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        ></input>
         <button onClick={handleAddCount}>+</button>
       </div>
       <p>
@@ -47,6 +48,14 @@ const Counter = () => {
           ? `${count > 0 ? `${count} days from today is  ${date.toDateString()}` : `${Math.abs(count)} days ago from today is  ${date.toDateString()}`}`
           : `Today is ${date.toDateString()}`}
       </p>
+      <button
+        onClick={() => {
+          setCount((c) => 0);
+          setStep((s) => 1);
+        }}
+      >
+        Reset
+      </button>
     </div>
   );
 };
