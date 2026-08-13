02eac507473f8590f25799c7475abbda84c85f85
drayata <profirstindra@gmail.com>
2026-08-13 08:08:53 +0700

    adding more reusability on components with props

--- react-course/07-usePopcorn/src/index.js
+++ react-course/07-usePopcorn/src/index.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import StarElement from "./StarElement";
 // import "./index.css";
 // import App from "./App";
+
+function Bintang() {
+  const [rating, setRating] = useState(0);
+  return (
+    <div>
+      <StarElement color="green" setOnRating={setRating} />
+      <p>film is rated {rating}</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
     <StarElement maxRatings={10} />
+    <StarElement
+      maxRatings={5}
+      color="red"
+      size={30}
+      messages={["very bad", "bad", "okay", "good", "very good"]}
+    />
+    <Bintang />
   </React.StrictMode>,
 );
