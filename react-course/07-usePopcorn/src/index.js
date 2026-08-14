7b2e22d5026afebc5d4c66015df1ce13f4f13a2e
drayata <profirstindra@gmail.com>
2026-08-14 19:10:49 +0700

    useEffect to fetch API

--- react-course/07-usePopcorn/src/index.js
+++ react-course/07-usePopcorn/src/index.js
@@ -1,30 +1,30 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
-import StarElement from "./StarElement";
-// import "./index.css";
-// import App from "./App";
+// import StarElement from "./StarElement";
+import "./index.css";
+import App from "./App";
 
-function Bintang() {
-  const [rating, setRating] = useState(0);
-  return (
-    <div>
-      <StarElement color="green" setOnRating={setRating} />
-      <p>film is rated {rating}</p>
-    </div>
-  );
-}
+// function Bintang() {
+//   const [rating, setRating] = useState(0);
+//   return (
+//     <div>
+//       <StarElement color="green" setOnRating={setRating} />
+//       <p>film is rated {rating}</p>
+//     </div>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarElement maxRatings={10} />
+    <App />
+    {/* <StarElement maxRatings={10} />
     <StarElement
       maxRatings={5}
       color="red"
       size={30}
       messages={["very bad", "bad", "okay", "good", "very good"]}
     />
-    <Bintang />
+    <Bintang /> */}
   </React.StrictMode>,
 );
