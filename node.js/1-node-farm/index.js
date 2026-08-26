c1c2f587634a83f941d40ae89966efa65341c8f1
drayata <profirstindra@gmail.com>
2026-08-26 21:20:57 +0700

    starting backend development

---
+++ node.js/1-node-farm/index.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const http = require("http");
+
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
+
+// const textOut = `Apa itu avocasdos from mexico? ${textIn} \n Created in ${Date.now().toLocaleString()}`;
+// fs.writeFileSync("./txt/output.txt", "Goblog lu");
+// console.log("File Written");
+
+http.createServer((req, res) => {});
