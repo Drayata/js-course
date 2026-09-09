0874e0963c010594fdc119664a924592270df713
drayata <profirstindra@gmail.com>
2026-09-09 14:10:30 +0700

    syncc 2

--- pern-course/ngetes/backend/index.js
+++ pern-course/ngetes/backend/index.js
@@ -1,10 +1,17 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
 
+app.use(cors());
+
 app.get("/", (req, res) => {
-  res("heylonn");
+  res.send("Wellcome");
 });
 
-app.listen(port, () => `Listening on port: ${port}`);
+app.get("/id", (req, res) => {
+  res.send({ nama: "jokowi", umur: 30, alamat: "pekalongan" });
+});
+
+app.listen(port, () => console.log(`Listening on port: ${port}`));
